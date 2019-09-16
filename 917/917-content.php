14303cfa6fb54d138be8cc4882a47894699c23ef
hoangtuanthai17 <44045204+hoangtuanthai17@users.noreply.github.com>
2019-09-16 20:42:32 +0700

    xóa phần head

--- 917/917-content.php
+++ 917/917-content.php
@@ -2,30 +2,8 @@
 
 <html>
 
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/animate.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <?php
-        if (!class_exists('lessc')) {
-            include ('./libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/type-1320.less', 'css/type-1320.css');
-        ?>
-        <link href="css/type-1320.css" rel="stylesheet" type="text/css" />
-
-        <script src="js/jquery.min.js" type="text/javascript"></script>
-        <script src="js/owl.carousel.min.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-
-    </head>
-
     <body>
-        <div class="type-1320">
+        <div class="type-917">
             <div id="first-slider">
                 <div id="carousel-example-generic" class="carousel slide carousel-fade">
                     <!-- Indicators -->

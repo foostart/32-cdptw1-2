2100b4b5a38f764c9fc42aa695a3ba32bfe89218
hoangtuanthai17 <44045204+hoangtuanthai17@users.noreply.github.com>
2019-09-24 10:46:50 +0700

    upload module1635

--- 1635/1635-content.php
+++ 1635/1635-content.php
@@ -1,57 +1,49 @@
-<!DOCTYPE html>
-      <section class="type-1635">
-               <div class="type-1635">
-                  <div class="shop title-size-default">
-                     <h1> SHOP </h1>
-                     <div><a href="#">Home</a> 
-                        <span></span>
-                        <a href="#">Shop</a>
-                     </div>
-                  </div>
-         <div class="row">
-            <div class="col-md-12">
-                  <div class="col-md-6">
-                     <div class="viewmode">
-                     <div class="container">
-                        <div class="shop-action">
-                           <div class="shop-action-inner flex between-xs">
-                              <div class="wc-switch flex">
-                                 <span class="mr_10">View mode:</span>
-                                 <a href="#" class="per-row-4 grid">
-                                 <i class="fa fa-th fa-lg"></i>
-                                 </a>
-                                 <a href="#" class="per-row-1 list active">
-                                 <i class="fa fa-th-list fa-lg"></i>
-                                 </a>
-                              </div>
-                           </div>
+   <div class="type-1635">
+         <div class="shop">
+            <h1> SHOP </h1>
+               <div><a href="#">Home</a> 
+                  <span></span>
+                  <a href="#">Shop</a>
+               </div>
+         </div>
+      <div class="row">
+         <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="viewmode">
+               <div class="container">
+                  <div class="shop-action">
+                        <div class="wc-switch flex">
+                           <span class="mr_10">View mode:</span>
+                           <a href="#" class="per-row-4 grid">
+                           <i class="fa fa-th fa-lg"></i>
+                           </a>
+                           <a href="#" class="per-row-1 list active">
+                           <i class="fa fa-th-list fa-lg"></i>
+                           </a>
                         </div>
-                     </div>
-                      </div>
                   </div>
-                  <div class="col-md-6" style="margin-top: 45px;">
-                              <div class="action-right flex middle-xs">
-                                 <form class="sort" method="get">
-                                    <span>Sort by:</span>
-                                    <select name="sortby">
-                                       <option value="Sort by 1">Sort by 1</option>
-                                       <option value="Sort by 2">Sort by 2</option>
-                                       <option value="Sort by 3">Sort by 3</option>
-                                    </select>
-                                 </form>
-                                 <form class="show" method="get">
-                                    <span>Show:</span>
-                                    <select name="showproducts">
-                                       <option value="6">6 </option>
-                                       <option value="8">8 </option>
-                                       <option value="10">10 </option>
-                                    </select>
-                                 </form>
-                           </span>
-                        </div>
-                      </div>
-                  </div>		
-                  </div>
-                  </div>		
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="action-right flex middle-xs">
+               <form class="sort" method="get">
+                  <span>Sort by:</span>
+                  <select name="sortby">
+                     <option value="Sort by 1">Sort by 1</option>
+                     <option value="Sort by 2">Sort by 2</option>
+                     <option value="Sort by 3">Sort by 3</option>
+                  </select>
+               </form>
+               <form class="show" method="get">
+                  <span>Show:</span>
+                  <select name="showproducts">
+                     <option value="6">6 </option>
+                     <option value="8">8 </option>
+                     <option value="10">10 </option>
+                  </select>
+               </form>
+            </div>
+         </div>	
          <script src="js/javascript.js"></script>
-      </section>
+      </div>
+   </div>
